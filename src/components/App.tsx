68aafdcaf896419fcabcab3a44a98a998e2e9475
João Gabriel <joaobielsabarreto@gmail.com>
2025-10-20 22:58:34 -0300

    Adicionando main.ts e App.tsx como estrutura inicial

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <h1>Bem-vindo ao Ranotify!</h1>
+      <p>Este é o componente principal do meu aplicativo.</p>
+    </div>
+  );
+};
+
+export default App;
